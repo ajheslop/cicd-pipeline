4e222c6eda93b995e8c419a079e7cf558ae978b2
ajheslop <72602212+ajheslop@users.noreply.github.com>
2021-07-20 11:23:53 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import json
 import csv
 import boto3
@@ -15,8 +17,10 @@ def lambda_handler(event, context):
         key = event['Records'][0]['s3']['object']['key']
 
         csv_file = s3.get_object(Bucket=bucket, Key=key)
-        record_list = csv_file['Body'].read().decode('utf-8').split('\n')
-        csv_reader = csv.reader(record_list, delimiter=',', quotechar='"')
+        record_list = csv_file['Body'].read().decode('utf-8').split('\n'
+                )
+        csv_reader = csv.reader(record_list, delimiter=',',
+                                quotechar='"')
 
         for row in csv_reader:
             student_id = row[0]
@@ -29,25 +33,23 @@ def lambda_handler(event, context):
             student_email = row[7]
             student_grade = row[8]
 
-            add_to_db = dynamodb.put_item(
-                TableName='students',
-                Item={
-                    'student_id': {'S': str(student_id)},
-                    'student_fname': {'S': str(student_fname)},
-                    'student_lname': {'S': str(student_lname)},
-                    'student_dob': {'S': str(student_dob)},
-                    'student_sex': {'S': str(student_sex)},
-                    'student_form': {'S': str(student_form)},
-                    'student_year': {'S': str(student_year)},
-                    'student_email': {'S': str(student_email)},
-                    'student_grade': {'S': str(student_grade)},
+            add_to_db = dynamodb.put_item(TableName='students', Item={
+                'student_id': {'S': str(student_id)},
+                'student_fname': {'S': str(student_fname)},
+                'student_lname': {'S': str(student_lname)},
+                'student_dob': {'S': str(student_dob)},
+                'student_sex': {'S': str(student_sex)},
+                'student_form': {'S': str(student_form)},
+                'student_year': {'S': str(student_year)},
+                'student_email': {'S': str(student_email)},
+                'student_grade': {'S': str(student_grade)},
                 })
+
         # rows in csv are split by lines
-            print(add_to_db)
-            print('sucessfully added the records to the dynamodb table')
-    except Exception as e:
-        print(str(e))
-    return {
-        'statusCode': 200,
-        'body': json.dumps('CSV to DynamoDB Success')
-    }
+
+            print add_to_db
+            print 'sucessfully added the records to the dynamodb table'
+    except Exception, e:
+        print str(e)
+    return {'statusCode': 200,
+            'body': json.dumps('CSV to DynamoDB Success')}
