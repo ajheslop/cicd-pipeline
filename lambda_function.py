6e67a2ecea62290831939400a95c431f82431a27
ajheslop <72602212+ajheslop@users.noreply.github.com>
2021-07-20 09:50:07 +0100

    update to linting

--- lambda_function.py
+++ lambda_function.py
@@ -1,17 +1,20 @@
 import sys
 import json
 
+
 def my_func(string):
     print('hello from lambda')
     print(sys.version)
     return string.upper()
 
+
 def lambda_handler(event, context):
     print(my_func("foo"))
 
-    return { 
+    return {
 
         'statusCode': 200,
         'headers': {},
+        
         'body': json.dumps(event)
     }
