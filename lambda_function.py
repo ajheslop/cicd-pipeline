5b4ddff5f0214b82b9fe12dd64c2ed728e136185
ajheslop <72602212+ajheslop@users.noreply.github.com>
2021-07-20 11:24:57 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -47,7 +47,7 @@ def lambda_handler(event, context):
 
         # rows in csv are split by lines
 
-            print add_to_db
+            print (add_to_db)
             print 'sucessfully added the records to the dynamodb table'
     except Exception, e:
         print str(e)
