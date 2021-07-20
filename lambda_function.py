2aeaf40e38aeecfb2f8ac51696d74dc2a101e1c4
Heslop <aheslop@dxc.com>
2021-07-20 10:08:45 +0100

    testing doc string

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,15 @@
 import sys
+"""System module."""
 import json
 
 
 def my_func(string):
+    """System module."""
     print('hello from lambda')
     print(sys.version)
     return string.upper()
 
-
+"""System module."""
 def lambda_handler(event, context):
     print(my_func("foo"))
 
