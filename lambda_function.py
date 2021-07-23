874ad4afb4b82d04bfef4aeed7d182aaef46dd88
ajheslop <72602212+ajheslop@users.noreply.github.com>
2021-07-23 13:49:34 +0100

    Lambda_function.flake8-completed

--- lambda_function.py
+++ lambda_function.py
@@ -7,14 +7,14 @@ class Calculator:
         """return string"""
         return num1 + num2
 
-    def subtract(self, num1, num2):
+    def subtract(num1, num2):
         """return string"""
         return num1 - num2
 
-    def divide(self, num1, num2):
+    def divide(num1, num2):
         """return string"""
         return num1 / num2
 
-    def multiply(self, num1, num2):
+    def multiply(num1, num2):
         """return string"""
         return num1 * num2
