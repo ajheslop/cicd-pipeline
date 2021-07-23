7c8ddd60a5159700128d33fd8861e0cfaa0847d2
ajheslop <72602212+ajheslop@users.noreply.github.com>
2021-07-23 14:13:51 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,6 @@
-"""Add two numbers"""
+""" Simple python function for testing out GitHub CI process """
 
 
-class Person:
-    """ Simple python function for testing out GitHub CI process """
-    def allowed_to_buy_alcohol(self, birthday, alcohol_percentage):
-        """ Simple python function for testing out GitHub CI process """
-        # age 16-18: < 16.5%
-        # age 18 and over: > 16.5%
-        pass
-
-    def allowed_to_buy_tobacco(self, birthday):
-        """ Simple python function for testing out GitHub CI process """
-        # age 18 and over
-        pass
+def basic_function():
+    """return string"""
+    return "hello world"
