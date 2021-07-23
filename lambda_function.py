0c6350fe3327ff6c05495e4a85e137242dc6787e
ajheslop <72602212+ajheslop@users.noreply.github.com>
2021-07-23 14:07:45 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,20 +1,11 @@
 """Add two numbers"""
 
+class Person:
+    def allowed_to_buy_alcohol(self, birthday, alcohol_percentage):
+        # age 16-18: < 16.5%
+        # age 18 and over: > 16.5%
+        pass
 
-class Calculator:
-
-    def add(self, num1, num2):
-        """Add two numbers"""
-        return num1 + num2
-
-    def subtract(self, num1, num2):
-        """Add two numbers"""
-        return num1 - num2
-
-    def divide(self, num1, num2):
-        """Add two numbers"""
-        return num1 / num2
-
-    def multiply(self, num1, num2):
-        """Add two numbers"""
-        return num1 * num2
+    def allowed_to_buy_tobacco(self, birthday):
+        # age 18 and over
+        pass
