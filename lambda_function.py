1f1e6de6d751eca703132d4a52b2ab77bae6eb6f
ajheslop <72602212+ajheslop@users.noreply.github.com>
2021-07-23 13:44:44 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,13 +4,17 @@
 class Calculator:
 
     def add(self, num1, num2):
+        """return string"""
         return num1 + num2
 
     def subtract(self, num1, num2):
+        """return string"""
         return num1 - num2
 
     def divide(self, num1, num2):
+        """return string"""
         return num1 / num2
 
     def multiply(self, num1, num2):
+        """return string"""
         return num1 * num2
