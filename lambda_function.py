33879908d627f14afb983d0b119a55834821d6c8
Heslop <aheslop@dxc.com>
2021-07-23 11:57:27 +0100

    test the new version

--- lambda_function.py
+++ lambda_function.py
@@ -1,53 +1,13 @@
-import json
-import csv
-import boto3
+class Calculator:
+    
+    def add(self, num1, num2):
+        return num1 + num2
+    
+    def subtract(self, num1, num2):
+        return num1 - num2
 
+    def divide(self, num1, num2):
+        return num1 / num2
 
-def lambda_handler(event, context):
-    region = 'eu-west-2'
-    record_list = []
-
-    try:
-        s3 = boto3.client('s3')
-        dynamodb = boto3.client('dynamodb', region_name=region)
-
-        bucket = event['Records'][0]['s3']['bucket']['name']
-        key = event['Records'][0]['s3']['object']['key']
-
-        csv_file = s3.get_object(Bucket=bucket, Key=key)
-        record_list = csv_file['Body'].read().decode('utf-8').split('\n')
-        csv_reader = csv.reader(record_list, delimiter=',', quotechar='"')
-
-        for row in csv_reader:
-            student_id = row[0]
-            student_fname = row[1]
-            student_lname = row[2]
-            student_dob = row[3]
-            student_sex = row[4]
-            student_form = row[5]
-            student_year = row[6]
-            student_email = row[7]
-            student_grade = row[8]
-
-            add_to_db = dynamodb.put_item(
-                TableName='students',
-                Item={
-                    'student_id': {'S': str(student_id)},
-                    'student_fname': {'S': str(student_fname)},
-                    'student_lname': {'S': str(student_lname)},
-                    'student_dob': {'S': str(student_dob)},
-                    'student_sex': {'S': str(student_sex)},
-                    'student_form': {'S': str(student_form)},
-                    'student_year': {'S': str(student_year)},
-                    'student_email': {'S': str(student_email)},
-                    'student_grade': {'S': str(student_grade)},
-                })
-        # rows in csv are split by lines
-            print(add_to_db)
-            print('sucessfully added the records to the dynamodb table')
-    except Exception as e:
-        print(str(e))
-    return {
-        'statusCode': 200,
-        'body': json.dumps('CSV to DynamoDB Success')
-    }
+    def multiply(self, num1, num2):
+        return num1 * num2
---
+++ test.py
@@ -0,0 +1,49 @@
+import unittest
+from lambda_function import Calculator
+
+class Tests(unittest.TestCase):
+
+    def setUp(self):
+        self.calculator = Calculator()
+
+    def test_calculator_object(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_add_function_exists(self):
+        self.calculator.add
+
+    def test_add_two_numbers(self):
+        result = self.calculator.add(10,2)
+        expected = 12
+
+        self.assertEqual(result, expected)
+
+    def test_subtract_function_exists(self):
+        self.calculator.subtract
+
+    def test_subtract_two_numbers(self):
+        result = self.calculator.subtract(10,2)
+        expected = 8
+
+        self.assertEqual(result, expected)
+
+    def test_divide_function_exists(self):
+        self.calculator.divide
+    
+    def test_divide_two_numbers(self):
+        result = self.calculator.divide(10,2)
+        expected = 5
+
+        self.assertEqual(result, expected)
+
+    def test_multiply_function_exists(self):
+        self.calculator.multiply
+
+    def test_multiply_two_numbers(self):
+        result = self.calculator.multiply(10,2)
+        expected = 20
+
+        self.assertEqual(result, expected)
+
+if __name__ == "__main__":
+    unittest.main() 
